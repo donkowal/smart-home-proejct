1ad13566b9b908055ddd304c321ab38cdd379822
donkowal <guadzisko@gmail.com>
2025-09-02 18:41:22 +0200

    modified:   PCcontroller.py 	modified:   __pycache__/PCcontroller.cpython-311.pyc

--- PCcontroller.py
+++ PCcontroller.py
@@ -1,5 +1,6 @@
 from wakeonlan import send_magic_packet
 
 def PC_on():
-    send_magic_packet('ff.ff.ff.ff.ff.ff') #adres MAC komputera
+    send_magic_packet('4c:cc:6a:f5:59:b6') #adres MAC komputera
+    print("wyslano magick packet3")
     return "PC turned on"
